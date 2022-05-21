c165bd2b90440eddfa927ba69f86c83af76af692
EL225 <ericleonmora99@gmail.com>
2022-05-21 03:06:43 +0000

    terminando

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [todo, setTodo] = useState([]);
+	const [isX, setIsX] = useState(false);
+	const [nuevatarea, setNuevatarea] = useState("");
+	const handleKeyEnter = (e) => {
+		let newTodo = [...todo, nuevatarea];
+		if (e.key === "Enter") {
+			setTodo(newTodo);
+			setNuevatarea("");
+		}
+	};
+
+	const borrado = (indice) => {
+		const Ntodo = todo.filter((item, index) => indice !== index);
+		setTodo(Ntodo);
+	};
+
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div style={{ marginTop: "10px" }}>
+				<h1 className="text-center">to dos</h1>
+			</div>
+			<div>
+				<input
+					type="text"
+					placeholder="to do"
+					value={nuevatarea}
+					onChange={(e) => {
+						setNuevatarea(e.target.value);
+					}}
+					onKeyDown={handleKeyEnter}></input>
+			</div>
+			<ul className="list-group">
+				{todo.map((item, index) => (
+					<li
+						onMouseOver={() => setIsX(true)}
+						onMouseLeave={() => setIsX(false)}
+						key={index}
+						className="list-group-item"
+						style={{
+							display: "flex",
+							justifyContent: "space-between",
+						}}>
+						{item}
+						{isX && (
+							<button
+								className="btn btn-danger"
+								onClick={() => borrado(index)}>
+								X
+							</button>
+						)}
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
